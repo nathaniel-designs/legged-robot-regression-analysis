19895f1ce6abf657d0ca6813dcf0f395baac0813
Destin Augustin <destinaug@gmail.com>
2025-11-18 14:14:35 -0500

    Updated with new variables, regression assumption plots, and model selection.

--- roboanalysis.r
+++ roboanalysis.r
@@ -1,5 +1,7 @@
 #install.packages("readr")
 library(readr)
+library(car)
+
 
 robot_data <- read_csv("robot_dataset.csv")
 
@@ -7,6 +9,8 @@ robot_data <- read_csv("robot_dataset.csv")
 #X = left-right, Y = forward-backward
 head(robot_data, 10)
 summary(robot_data)
+colSums(is.na(robot_data))
+#No missing data! 
 
 avg_angularmag <- mean(robot_data$angular_mag)
 median_angularmag <- median(robot_data$angular_mag)
@@ -28,13 +32,36 @@ data_exploration_df <- data.frame(
   JerkMag = c(avg_jmag, median_jmag, stddev_jmag, min_jmag, max_jmag, range_jmag)
 )
 
+print(data_exploration_df)
+robot_data$Environment <- as.factor(robot_data$Environment)
+levels(robot_data$Environment)
+
+hist(robot_data$angular_mag)
+hist(robot_data$jmag)
+
+hist(log(robot_data$angular_mag))
+hist(log(robot_data$jmag))
+
 plot(robot_data$angular_mag, robot_data$jmag, xlab = "Angular Magnitude", ylab = "Jerk Magnitude")
 plot(robot_data$angular_mag, robot_data$jmag, xlab = "Angular Magnitude", ylab = "Jerk Magnitude", log='xy')
 
-model <- lm((robot_data$jmag) ~ robot_data$angular_mag + robot_data$Environment + (robot_data$angular_mag * robot_data$Environment))
+boxplot(formula = jmag ~ Environment, data = robot_data, main = "Box Plot of Jerk Magnitude by Environment", xlab = "Environment", ylab = "Jerk Magnitude")
+
+model <- lm(log(jmag) ~ log(angular_mag) + Environment + log(angular_mag):Environment, data = robot_data)   
 summary(model)
 anova(model)
 
 plot(model$fitted.values, residuals(model), xlab = "Predicted Values", ylab = "Residuals",)
 Residuals <- residuals(model)
 hist(Residuals, breaks = 30, xlab = "Residuals")
+
+qqnorm(residuals(model))
+qqline(residuals(model), col = "red")
+vif(model)
+plot(model, 4, id.n = 5)
+which(cooks.distance(model) > 4/length(model$fitted.values))
+
+model_reduced <- lm(log(jmag) ~ log(angular_mag) + Environment, data = robot_data)
+
+AIC(model, model_reduced)
+BIC(model, model_reduced)
